be612eb4d4672239cd9969bd19d64b93e6fad2a7
Domenico Panella <pandom79@gmail.com>
2022-05-19 11:49:28 +0200

    Add datetime-wrapper.c

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
-#include <stdio.h>
+#include "wrapper.h"
 
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+//    time_t now = time(NULL);
+//    struct tm *localTime = localtime(&now);
+//    printf("localTime->tm_sec = %d\n", localTime->tm_sec);
+//    printf("localTime->tm_min = %d\n", localTime->tm_min);
+
+////    localTime->tm_mday -= 1;
+////    localTime->tm_hour -= 2;
+//    localTime->tm_sec -= 1;
+////    localTime->tm_min -= 1;
+
+//    printf("localTime->tm_sec = %d\n", localTime->tm_sec);
+//    printf("localTime->tm_min = %d\n", localTime->tm_min);
+
+//    time_t start = mktime(localTime);
+
+//    time_t now2 = time(NULL);
+
+//    double diffTime = difftime(now2, start);
+//    printf("diffTime = %0.0f\n", diffTime);
+
+//    printDiffTime(diffTime);
+
+    time_t now = time(NULL);
+
+    msleep(500);
+
+    time_t after = time(NULL);
+
+    printDiffTime(difftime(after, now));
+
 }
